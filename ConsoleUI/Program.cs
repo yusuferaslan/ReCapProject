d918803100257094f24ddd081f11d43153aa38ba
yusuf <yusuferaslan@hotmail.com>
2023-05-30 00:10:16 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccsess.Concrete.InMemory;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+        }
+    }
+}
